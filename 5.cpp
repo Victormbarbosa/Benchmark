c811ffd57ba198334eb4b0abd183e61dd9653537
Andrés David <adpg.611@gmail.com>
2020-02-15 20:10:13 -0500

    Archivos

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+int main() {
+    // Programas para cada benchmark
+    // CPU: Encontrar los primeros 50000 números primos con 10 hilos.
+    // CPU: Encontrar los primeros 250000 dígitos de pi.
+    // CPU: Encontrar la salida del laberinto con X hilos.
+    // Disco: Duración de escritura (archivo generado) y lectura y de archivos de 1, 10, 100, 500 y 1000 MB.
+    // Disco: Medición de cuantos archivos de 4 Kb puede copiar de una carpeta a otra en X segundos.
+    // RAM: Iterar sobre diferentes cantidades (hasta 2 Gb) de datos aleatorios
+
+    return 0;
+}
