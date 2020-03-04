9a890221c7f7f1c58bee3d8128746ebc0248c9c9
Andrés David <adpg.611@gmail.com>
2020-03-03 19:22:11 -0500

    Programa para hallar 50k números primos

--- primos.cpp
+++ primos.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <thread>
+#include <iostream>
 #include <math.h>
-
 using namespace std;
 
 void encontrar_primos(int comienzo, int rango) {
@@ -20,21 +19,14 @@ void encontrar_primos(int comienzo, int rango) {
 }
 
 int main() {
-    // Programas para cada benchmark
-
-    // CPU: Encontrar los primeros 50000 números primos con 10 hilos.
     int N = 50000, H = 10, rango = N/H;
     thread hilos[H];
+
     for (int i = 0; i < H; i++)
         hilos[i] = thread(encontrar_primos, 1 + rango*i, rango);
+
     for (int i = 0; i < H; i++)
         hilos[i].join();
-
-    // CPU: Encontrar los primeros 250000 dígitos de pi.
-    // CPU: Encontrar la salida del laberinto con X hilos.
-    // Disco: Duración de escritura (archivo generado) y lectura y de archivos de 1, 10, 100, 500 y 1000 MB.
-    // Disco: Medición de cuantos archivos de 4 Kb puede copiar de una carpeta a otra en X segundos.
-    // RAM: Iterar sobre diferentes cantidades (hasta 2 Gb) de datos aleatorios
 
     return 0;
 }
